67920b3a8e640682e368e3782c2ae1c0c5abdb7b
Daria Nikolaeva <nikolaevdaaa@gmail.com>
2025-02-10 18:11:38 +0300

    Initial Commit

---
+++ ShoppingAppTests/ShoppingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppTests.swift
+//  ShoppingAppTests
+//
+//  Created by Даша Николаева on 10.02.2025.
+//
+
+import Testing
+@testable import ShoppingApp
+
+struct ShoppingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
